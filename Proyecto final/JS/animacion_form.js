e3912ae08775a8681ce0526c995d49ebdb03fed5
mirkolino2002 <129808114+mirkolino2002@users.noreply.github.com>
2023-04-11 15:14:17 -0300

    Initial commit

---
+++ Proyecto final/JS/animacion_form.js
@@ -0,0 +1,18 @@
+
+/*const $form = document.querySelector("#form");
+const $button= document.querySelector("#mail");
+
+$form.addEventListener('submit', handleSubmit)
+function handleSubmit(event){
+  event.preventDefault()
+  const form = new FormData(this)
+
+  $button.setAttribute('href', `mailto:mirwis.2002@gmail.com?subject= nombre ${form.get('nombres')} correo ${form.get("correo")}&body = mensaje ${form.get("message")}`)
+  $button.click()
+  if ($button == $button.click())
+    alert("Tu mensaje se ha enviado correctamente")
+}*/
+
+/* Este archivo hace la función de que funcione el formulario y se diriga 
+y se envíe un mensaje a un correo en particular. 
+En este caso lo dejaré inhabilitado.*/
